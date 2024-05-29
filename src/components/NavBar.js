4ef35b5e0f03db3ef6d2259ffbb6732eac0a94e5
Suswetha <suswetha.23bcs10166@ms.sst.com>
2024-05-29 13:29:08 +0530

    HomePage created

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+function NavBar(){
+  return (
+    <>
+      <nav className="navbar">
+        <ul>
+          <li className='nav-item'><a >Home</a></li>
+          <li className='nav-item'><a >About</a></li>
+          <li className='nav-item'><a >Project</a></li>
+          <li className='nav-item'><a >Resume</a></li>
+        </ul>
+      </nav>
+    
+    </>
+  )
+}
+export default NavBar
