e031e9607d9335906657b5e47d725cc6bf415782
Suswetha <suswetha.23bcs10166@ms.sst.com>
2024-07-26 17:42:37 +0530

    linked Resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar(){
           <li className='nav-item'><Link to="/education">Education</Link></li>
           <li className='nav-item'><Link to="/about">About</Link></li>
           <li className='nav-item'><Link to="/project">Projects</Link></li>
-          <li className='nav-item'><a >Resume</a></li>
+          <li className='nav-item'><a href="https://drive.google.com/file/d/1VkjwkufjxpAuQCyIeUfZDO3MzaInfAIA/view?usp=sharing" target="_blank" rel="noopener noreferrer" >Resume</a></li>
         </ul>
       </nav>
     
